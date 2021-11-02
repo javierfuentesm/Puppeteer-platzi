3486fd8fd1e0ed28ff7f3e87c642732a9da2b18f
Javier <javier.fuentesm@hotmail.com>
2021-11-02 13:11:20 -0400

    Correccion

--- __tests__/navegador.test.js
+++ __tests__/navegador.test.js
@@ -4,18 +4,10 @@ describe('Mi primer test de puppeteer', () => {
 	it('Debe abrir y cerrar el navegador el navegador', async () => {
 		const browser = await puppeteer.launch({
 			headless: false,
-			slowMo: 0,
-			devtools: true,
-			// defaultViewport: {
-			// 	width: 2100,
-			// 	height: 1080,
-			// },
-			args: ['--window-size=1920,1080'],
-			defaultViewport: null,
 		})
 		const page = await browser.newPage()
 		await page.goto('http://automationpractice.com/index.php')
 		await page.waitForSelector('h1')
 		await browser.close()
-	}, 30000)
+	}, 10000)
 })
